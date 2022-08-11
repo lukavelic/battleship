95852be2a0dc6c8b0ddca573d58da7e416a80655
lukavelic <96191586+lukavelic@users.noreply.github.com>
2022-08-11 12:06:43 +0200

    Clean up index.js

--- src/index.js
+++ src/index.js
@@ -1,40 +1,10 @@
-import { ShipFactory } from "./ship.js";
-import { PlayerFactory } from "./player.js";
-import { GameboardFactory } from "./gameboard.js";
 import { GameFactory } from "./game.js";
 import { RenderFactory } from "./render.js";
-
-console.log('test');
 
 // INITIAL SETUP
 
 const renderDOM = RenderFactory();
 const game = GameFactory();
-renderDOM.initializeSubmitButton();
-
-// renderDOM.renderUI();
-// renderDOM.renderBoard(1);
-// renderDOM.renderBoard(2);
-
-// game.createPlayer('luka', 1);
-// game.createShip('battleship', 5, 5, 1);
-// console.log(game)
-// game.checkTile(5,5);
-// console.log(game.checkTile(5,5))
+renderDOM.initializeSubmitButton(); 
 
 export {game, renderDOM};
-
-// const battleship = ShipFactory('battleship', 2, 3, 0);
-// console.log(battleship);
-// console.log(battleship.getStatus());
-// console.log(battleship.getPosition())
-
-// const gameboard = GameboardFactory();
-
-// export { gameboard };
-
-/////////////////// PROJECT OUTLINE //////////////////
-
-// Press square
-// Get X and Y coordinates
-// 
